1ebcae3c14e983d908ecb77d27b0c380e0c8099e
PremalathaKumar <premalatha@webulous.in>
2018-09-12 12:37:47 +0530

    Gutenberg Style unit added

--- footer.php
+++ footer.php
@@ -26,9 +26,7 @@
 				<?php if( get_theme_mod('copyright') ) : ?>
 							<p><?php echo primal_footer_copyright(get_theme_mod('copyright')); ?></p>
 						<?php else : 
-								printf( __('<p>Powered by <a href="%1$s" target="_blank">WordPress</a>', 'primal'), esc_url( 'http://wordpress.org/') );
-								printf( '<span class="sep"> .</span>' );
-								printf( __( 'Theme: Primal by <a href="%1$s" target="_blank" rel="designer">Genex Themes</a></p>', 'primal' ), esc_url('http://www.genexthemes.com/') );
+							echo sprintf( '<p> %1$s <a href="%2$s" target="_blank"> %3$s</a> %4$s <a href="%5$s" target="_blank" rel="designer">%6$s</a></p>', __('Powered by','primal'), esc_url( 'http://wordpress.org/'), __('WordPress.','primal'), __('Theme: Primal by','primal'), esc_url('https://www.genexthemes.com/'), __('Genex Themes','primal')) ;
 					 endif;  ?>
 				</div>
 				<div class="left-sidebar eight columns">
